5bfdbfdfd86873beaa0ea41f47f6abd3b8d26a27
yusbel86 <yusbel1086@gmail.com>
2020-04-14 16:35:27 +0000

    Add login form

--- src/js/views/Account/Login.js
+++ src/js/views/Account/Login.js
@@ -1,9 +1,42 @@
 import React from "react";
+import "./login.css";
 
 export const Login = () => {
 	return (
-		<div>
-			<h1>Login</h1>
-		</div>
+		<form className="container form">
+			<div className="form-group">
+				<label for="exampleInputEmail1">Email address</label>
+				<input
+					type="email"
+					className="form-control"
+					id="exampleInputEmail1"
+					aria-describedby="emailHelp"
+				/>
+				<small id="emailHelp" className="form-text text-muted">
+					We'll never share your email with anyone else.
+				</small>
+			</div>
+			<div className="form-group">
+				<label for="exampleInputPassword1">Password</label>
+				<input
+					type="password"
+					className="form-control"
+					id="exampleInputPassword1"
+				/>
+			</div>
+			<div className="form-group form-check">
+				<input
+					type="checkbox"
+					className="form-check-input"
+					id="exampleCheck1"
+				/>
+				<label className="form-check-label" for="exampleCheck1">
+					Check me out
+				</label>
+			</div>
+			<button type="submit" className="btn btn-primary">
+				Submit
+			</button>
+		</form>
 	);
 };
