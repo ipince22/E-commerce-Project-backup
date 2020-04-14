d2af4cea6ab6705f4dc35270dddad0410e630106
Daniel Ipince <ipince22@gmail.com>
2020-04-14 04:09:20 +0000

    04.13 daniel

--- src/js/component/ProductList.js
+++ src/js/component/ProductList.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Product } from "./Product";
+import { Title } from "./Title";
+import { storeProducts } from "../../data/data";
 
 export const ProductList = () => {
+	const [products, setProducts] = useState();
+	//setProducts({ contacts: storeProducts });
+	//setProducts(storeProducts);
+	console.log("products-->", storeProducts);
 	return (
-		<div>
-			<Product />
-		</div>
+		<React.Fragment>
+			<div className="py-5">
+				<div className="container">
+					<Title name="our" title="products" />
+					<div className="row" />
+				</div>
+			</div>
+		</React.Fragment>
+		//<Product />
 	);
 };
