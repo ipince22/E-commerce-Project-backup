c3da10afbcee906806387c089327ea5f8f557052
yusbel86 <yusbel1086@gmail.com>
2020-04-19 03:54:14 +0000

    Added checkout

--- src/js/views/Checkout/Checkout.js
+++ src/js/views/Checkout/Checkout.js
@@ -6,7 +6,8 @@ const Checkout = () => {
 		<div className="row">
 			<div className="col">
 				<form className="checkout-form">
-					<div type="form-group">
+					<h3>Shipping Address</h3>
+					<div className="form-group">
 						<label for="name">Name</label>
 						<input
 							type="text"
@@ -17,7 +18,7 @@ const Checkout = () => {
 						/>
 					</div>
 
-					<div type="form-group">
+					<div className="form-group">
 						<label for="name">Last Name</label>
 						<input
 							type="text"
@@ -28,7 +29,7 @@ const Checkout = () => {
 						/>
 					</div>
 
-					<div type="form-group">
+					<div className="form-group">
 						<label for="name">Address Line 1</label>
 						<input
 							type="text"
@@ -39,7 +40,7 @@ const Checkout = () => {
 						/>
 					</div>
 
-					<div type="form-group">
+					<div className="form-group">
 						<label for="name">Address Line 2</label>
 						<input
 							type="text"
@@ -50,7 +51,7 @@ const Checkout = () => {
 						/>
 					</div>
 
-					<div type="form-group">
+					<div className="form-group">
 						<label for="name">City</label>
 						<input
 							type="text"
@@ -63,20 +64,68 @@ const Checkout = () => {
 
 					<div className="row">
 						<div className="col">
-							<div type="form-group">
+							<div className="form-group">
 								<label for="name">State</label>
-								<input
-									type="text"
-									placeholder="State"
-									name="state"
-									id="state"
-									className="form-control"
-								/>
+								<select className="form-control" id="state">
+									<option value="AL">Alabama</option>
+									<option value="AK">Alaska</option>
+									<option value="AZ">Arizona</option>
+									<option value="AR">Arkansas</option>
+									<option value="CA">California</option>
+									<option value="CO">Colorado</option>
+									<option value="CT">Connecticut</option>
+									<option value="DE">Delaware</option>
+									<option value="DC">
+										District Of Columbia
+									</option>
+									<option value="FL">Florida</option>
+									<option value="GA">Georgia</option>
+									<option value="HI">Hawaii</option>
+									<option value="ID">Idaho</option>
+									<option value="IL">Illinois</option>
+									<option value="IN">Indiana</option>
+									<option value="IA">Iowa</option>
+									<option value="KS">Kansas</option>
+									<option value="KY">Kentucky</option>
+									<option value="LA">Louisiana</option>
+									<option value="ME">Maine</option>
+									<option value="MD">Maryland</option>
+									<option value="MA">Massachusetts</option>
+									<option value="MI">Michigan</option>
+									<option value="MN">Minnesota</option>
+									<option value="MS">Mississippi</option>
+									<option value="MO">Missouri</option>
+									<option value="MT">Montana</option>
+									<option value="NE">Nebraska</option>
+									<option value="NV">Nevada</option>
+									<option value="NH">New Hampshire</option>
+									<option value="NJ">New Jersey</option>
+									<option value="NM">New Mexico</option>
+									<option value="NY">New York</option>
+									<option value="NC">North Carolina</option>
+									<option value="ND">North Dakota</option>
+									<option value="OH">Ohio</option>
+									<option value="OK">Oklahoma</option>
+									<option value="OR">Oregon</option>
+									<option value="PA">Pennsylvania</option>
+									<option value="RI">Rhode Island</option>
+									<option value="SC">South Carolina</option>
+									<option value="SD">South Dakota</option>
+									<option value="TN">Tennessee</option>
+									<option value="TX">Texas</option>
+									<option value="UT">Utah</option>
+									<option value="VT">Vermont</option>
+									<option value="VA">Virginia</option>
+									<option value="WA">Washington</option>
+									<option value="WV">West Virginia</option>
+									<option value="WI">Wisconsin</option>
+									<option value="WY">Wyoming</option>
+								</select>
 							</div>
 						</div>
 
 						<div className="col">
-							<div type="form-group">
+							<div className="form-group">
 								<label for="name">Zip code</label>
 								<input
 									type="text"
@@ -89,7 +138,7 @@ const Checkout = () => {
 						</div>
 					</div>
 
-					<div type="form-group">
+					<div className="form-group">
 						<label for="name">Phone Number</label>
 						<input
 							type="text"
@@ -100,7 +149,7 @@ const Checkout = () => {
 						/>
 					</div>
 
-					<button type="submit" class="btn btn-primary">
+					<button type="submit" className="btn btn-primary">
 						Submit
 					</button>
 				</form>
