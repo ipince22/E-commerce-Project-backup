29fc0762d732a9202c8f7d829e2f0b9a345f31e3
yusbel86 <yusbel1086@gmail.com>
2020-04-22 03:36:39 +0000

    Added Sign_up

--- src/js/views/Account/Login.js
+++ src/js/views/Account/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 import { FacebookLoginButton } from "react-social-login-buttons";
+import { ButtonContainer } from "../../component/Button";
 import { Link } from "react-router-dom";
 import "./login.css";
 
@@ -18,19 +19,19 @@ export const Login = () => {
 				<Input type="password" placeholder="password" />
 			</FormGroup>
 
-			<Button className="btn-lg btn-dark btn-block">Log in</Button>
+			<ButtonContainer className="btn-lg btn-block">
+				Log in
+			</ButtonContainer>
 
 			<div className="text-center pt-3">Or continue with Facebook</div>
 
 			<FacebookLoginButton className="mt-3 mb-3" />
 
 			<div className="text-center">
-				<a href="/sign-up">Sign up</a>
+				<a href="/sign_up">Sign up</a>
 				<span className="p-2">|</span>
-				<a href="forgot password">Forgot Password</a>
+				<a href="password">Forgot Password</a>
 			</div>
-
-			<Link to="/checkout">Products</Link>
 		</Form>
 	);
 };
